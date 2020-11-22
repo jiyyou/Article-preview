b148293234198d8ff376505275f968c5f92abc84
Jiyo <ji.yyou@gmail.com>
2020-11-22 17:23:55 -0500

    script.js for adding classes on click, css update

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-let button = document.querySelector('.button');
+let button = document.querySelector('.card__button');
 let footer = document.querySelector('.card__footer');
-let shareBox = document.querySelector('card__sharebox');
+let shareBox = document.querySelector('.card__sharebox');
+let userBox = document.querySelector('.card__userbox');
 
+const toggleShare = () => {
+	button.addEventListener('click', e => {
+		e.preventDefault();
+		if (shareBox.classList.contains('card__sharebox--active')) {
+			shareBox.classList.remove('card__sharebox--active');
+			footer.classList.remove('card__footer--active');
+			button.classList.remove('card__button--active');
+			userBox.classList.remove('card__userbox--active');
+		}
+		else {
+			shareBox.classList.add('card__sharebox--active');
+			footer.classList.add('card__footer--active');
+			button.classList.add('card__button--active');			
+			userBox.classList.add('card__userbox--active');
+		}
+	});
+}
 
-button.addEventListener('click', e => {
-	e.preventDefault();
-
-})
+toggleShare();
