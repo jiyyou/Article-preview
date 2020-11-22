e940f41a52922f18a5caa02802d03d304fac0377
Jiyo <ji.yyou@gmail.com>
2020-11-22 15:55:45 -0500

    script.js initialize

---
+++ script.js
@@ -0,0 +1,9 @@
+let button = document.querySelector('.button');
+let footer = document.querySelector('.card__footer');
+let shareBox = document.querySelector('card__sharebox');
+
+
+button.addEventListener('click', e => {
+	e.preventDefault();
+
+})
